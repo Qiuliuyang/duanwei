57f58b551c012db55e98ab6924bb57cff55496f7
Qiuliuyang <381227470@qq.com>
2017-12-19 16:33:40 +0800

    add test

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+# __author__ = "Qiu"
+# Email = "qiu@immunet.cn"
+'''
+just for test
+'''
+import random
+list = range(1,10000)
+r_list = random.sample(list,10)
+print r_list
